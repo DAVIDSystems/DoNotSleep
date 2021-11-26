6825adf08ba7e200dbf1914829e45ede05ce3dca
DAVIDSystems <40237707+DAVIDSystems@users.noreply.github.com>
2021-11-26 09:01:36 +0100

    Delete donotsleep/Setup directory

--- donotsleep/Setup/SignSetup.bat
+++
@@ -1,4 +0,0 @@
-C:
-cd C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\
-signtool.exe sign /f G:\Data\Projects\git\CodeSigning\DavidSystemsCodeSigning.pfx /p DaViD2016_SignMe! /t http://timestamp.verisign.com/scripts/timstamp.dll %1
-timeout /T 10
